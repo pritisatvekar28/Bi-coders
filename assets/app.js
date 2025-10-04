025300b42f02e9ba4d5e7854036589ab187d809b
pritisatvekar28 <pritisatvekar28@gmail.com>
2025-10-04 12:24:34 +0530

    final commit

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,23 @@
 //function to fetch the meals 
-
+window.galerry = document.getElementById("galerry");
+const para = document.getElementById("category");
 $(document).ready(function(){
-  console.log("helcome")  
-})
-function search(value){
-    let query = "https://www.themealdb.com/api/json/v1/1/search.php?s="+value;
+  let query = "https://www.themealdb.com/api/json/v1/1/categories.php";
     $.get(query, function(data, status){
-        data.array.forEach(element => {
-           console.log(element.strMeal)
-        });
-    })
-    console.log()
+        var json = data.categories;
+    for(let i = 0; i < json.length; i++) {
+    let obj = json[i];
+
+    console.log(obj.strCategory);
+    const p = document.createElement("p"); 
+    p.id = "category";
+    p.innerHTML = obj.strCategory;
+   // p.href = "https://www.themealdb.com/api/json/v1/1/filter.php?c="+obj.strCategory;
+    galerry.appendChild(p);
 }
+    }) 
+})
+const pClick = document.querySelector();
+pClick.addEventListener("click", () => {
+    alert(this.innerHTML)
+})
